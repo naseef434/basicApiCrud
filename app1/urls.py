889912e0b0e080b1e64516efcf784a7a593c1143
naseef <naseefp.hin@gmail.com>
2022-05-26 21:37:58 +0530

    generics crud

--- app1/urls.py
+++ app1/urls.py
@@ -1,13 +1,10 @@
 
 from django.urls import path
 from . import views
-
-
-
-
-
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('album/',views.Albumview.as_view()),
     path('album/<int:pk>/',views.Albumview.as_view()),
 ]
+
--- app1/views.py
+++ app1/views.py
@@ -1,38 +1,70 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-
+from rest_framework import mixins
+from rest_framework import generics
 from .models import Album
 from .serializers import AlbumSerializer
 from rest_framework.response import Response
 
-class Albumview(APIView):    
-    def get_object(self,pk):
-        try:
-            print("HH")
-            Album.objects.get(id=pk)
-        except Exception as e:
-            print(e)    
+# class Albumview(APIView):    
+#     def get_object(self,pk):
+#         try:
+#             print("HH")
+#             Album.objects.get(id=pk)
+#         except Exception as e:
+#             print(e)    
         
-    def get(request,self,*args, **kwargs):
-        queryset = Album.objects.all()
-        serializer_class=AlbumSerializer(queryset,many=True)
+#     def get(request,self,*args, **kwargs):
+#         queryset = Album.objects.all()
+#         serializer_class=AlbumSerializer(queryset,many=True)
         
-        return Response(serializer_class.data)
-    def post(self, request, format=None):
+#         return Response(serializer_class.data)
+#     def post(self, request, format=None):
         
-        serializer=AlbumSerializer(data=request.data)
+#         serializer=AlbumSerializer(data=request.data)
         
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
     
-    def put(self,request,pk,format=None):
-        album=self.get_object(pk)
-        serializer = AlbumSerializer(album,data=request.data)
+#     def put(self,request,pk,format=None):
+#         album=self.get_object(pk)
+#         serializer = AlbumSerializer(album,data=request.data)
         
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
         
-        return Response(serializer.errors)
+#         return Response(serializer.errors)
+
+class Albumview(mixins.ListModelMixin,
+                mixins.CreateModelMixin,
+                mixins.RetrieveModelMixin,
+                mixins.UpdateModelMixin,
+                mixins.DestroyModelMixin,
+                generics.GenericAPIView):
+    
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+    lookup_field = 'pk'
+   
+    #List all 
+    def get(self,request,*arg, **kwargs):
+        return self.list(request,*arg, **kwargs)
+   
+    # create
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+   
+    # get single object
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(self, request, *args, **kwargs)
+    
+    # update
+    def put(self, request, *args, **kwargs):
+        return self.update(self, request, *args, **kwargs)
+    
+    # delete
+    def delete(self, request, *args, **kwargs):
+        return self(self, request, *args, **kwargs)
